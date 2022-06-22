98a83ece5c1a7462e4b08652eac8ec8fb7fa0145
Fahri Akbaba <fahriakbaba@gmail.com>
2022-06-22 22:48:24 +0300

    added a form to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = React.useState("");
+  const [list, setList] = React.useState([]);
+  const [editID, setEditID] = React.useState(null);
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [alert, setAlert] = React.useState({ show: false, msg: "", type: "" })
   return (
     <div className="App">
-       to do list app
+      <section className='container'>
+        <form className='todolist-form'>
+          <label htmlFor="todolist-input">to do list</label>
+          <input 
+            type="text" 
+            placeholder='Enter a duty' 
+            className='todolist-input' 
+            id='todolist-input' 
+          />
+          <button type='submit' className='todolist-btn'>submit</button>
+        </form>
+        <div className="list-container">
+          list container
+        </div>
+      </section>
     </div>
   );
 }
