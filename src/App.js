9e915b00c4a7b3cc9abb21dfdd0b7815dc67cf4e
Fahri Akbaba <fahriakbaba@gmail.com>
2022-06-25 16:59:57 +0300

    updated form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-
 import React from 'react';
 import './App.css';
+
 
 function App() {
   const [name, setName] = React.useState("");
   const [list, setList] = React.useState([]);
+
+{/*
   const [editID, setEditID] = React.useState(null);
   const [isEditing, setIsEditing] = React.useState(false);
   const [alert, setAlert] = React.useState({ show: false, msg: "", type: "" })
+*/}
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(!name) {
+      alert("Please, enter your duty!")
+    } else {
+      setList(prevState => ([...prevState, {id: Date.now().toString(), name:name}]))
+    }
+  }
+
+  console.log(list)
+
   return (
     <div className="App">
       <section className='container'>
-        <form className='todolist-form'>
+        <form className='todolist-form' onSubmit={handleSubmit}>
           <label htmlFor="todolist-input">to do list</label>
           <input 
             type="text" 
             placeholder='Enter a duty' 
             className='todolist-input' 
             id='todolist-input' 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <button type='submit' className='todolist-btn'>submit</button>
         </form>
         <div className="list-container">
           list container
         </div>
+  
       </section>
     </div>
   );
